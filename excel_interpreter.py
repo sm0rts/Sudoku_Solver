1b99c94b6a8c5dbc10bc0970c12348dc857c6907
Andrei Kuzin <Andrei.Kuzin@arm.com>
2024-08-11 16:17:46 +0100

    modified script to potentially add other input file types. created a placeholder class for cell, which will later contain all the necessary info to define each of the 81 cell objects

--- excel_interpreter.py
+++ excel_interpreter.py
@@ -2,10 +2,19 @@ import os
 import pandas as pd 
 import csv  
 
+class cell:
+    pass
+
 def print_current_sudoku(df):
-    df_formatted = df.applymap(lambda x: '{:.0f}'.format(x) if isinstance(x, (int, float)) else x)
+    df_formatted = df.map(lambda x: '{:.0f}'.format(x) if isinstance(x, (int, float)) else x)
     print(df_formatted)
 
+def convert_sudoku_to_data_frame(file_path):
+    file_ending = file_path.split(".")[-1]
+    if file_ending == "xlsx":
+        df = pd.read_excel("Sudoku.xlsx")
+    return(df)
+
 if __name__ == "__main__":
-    df = pd.read_excel("Sudoku.xlsx")
+    df = convert_sudoku_to_data_frame("Sudoku.xlsx")
     print_current_sudoku(df)
